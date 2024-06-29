8ae9946e6922ff990c6e9a3e4d2b96fd130a0643
Lychan23 <tester.yap84@gmail.com>
2024-06-29 14:15:32 +0700

    1.0.6

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f49cefa759491357377fdd787afacca44c1b7c2031504c4d2044cc69e358e29
-size 190
+oid sha256:71dc5371978f134574eeb99a8f30e09be5f2194c65259cf7c2c6e49285502581
+size 170
