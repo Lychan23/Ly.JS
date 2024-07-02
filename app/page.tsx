bcdc9e539b38c2a9b467771dd49cb28cc7939560
Lychan23 <tester.yap84@gmail.com>
2024-07-02 17:01:20 +0700

    test-new-folder structure-pre-release not functioned

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+// app/page.tsx
+import React from 'react';
+import Layout from './layout';
+
+const HomePage: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Welcome to Ly.JS Project</h1>
+      <p>This is the homepage of the project. Navigate through the pages to learn more.</p>
+    </Layout>
+  );
+};
+
+export default HomePage;
+
